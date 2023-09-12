14eb8530488468de8ec866c9c39955a10385785a
EllieNguyen <ellieN1235@gmail.com>
2023-09-12 00:49:42 -0700

    Initial Commit

---
+++ MoveMint/MoveMintApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoveMintApp.swift
+//  MoveMint
+//
+//  Created by Ellie on 9/12/23.
+//
+
+import SwiftUI
+
+@main
+struct MoveMintApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
